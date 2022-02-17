296e70fcbec99cbc838857796d8cddd45e3318b0
Neha <nehakumari16.70@gmail.com>
2022-02-17 18:17:58 +0530

    initial commit

---
+++ aboutus.php
@@ -0,0 +1,65 @@
+<?php session_start();?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<?php
+			include("includes/head.inc.php");
+		?>
+</head>
+
+<body>
+			<!-- start header -->
+				<div id="header">
+					<div id="menu">
+						<?php
+							include("includes/menu.inc.php");
+						?>
+					</div>
+				</div>
+				
+				<div id="logo-wrap">
+					<div id="logo">
+							<?php
+								include("includes/logo.inc.php");
+							?>
+					</div>
+				</div>
+			<!-- end header -->
+			
+			<!-- start page -->
+
+				<div id="page">
+					<!-- start content -->
+					<div id="content">
+						<div class="post">
+							<h1 class="title">About Us</h1>
+							<div class="entry" style="height:auto">
+								
+							Our philosophy is based on information, communication and education, which we feel should be balanced out with a daily dose of knowledge, wisdom, understanding.<br/>You can feel good about you purchase when you shop here.
+							Our website features books from every literary genre, allowing us to appeal to any particular taste a customer may have.
+ <br/>Most of their books are excess inventory tha they ge directly from publishers at a big discount. The books have a huge variety of products on the online store.
+							</div>
+							
+						</div>
+						
+					</div>
+					<!-- end content -->
+					
+					<!-- start sidebar -->
+					<div id="sidebar">
+							<?php
+								include("includes/search.inc.php");
+							?>
+					</div>
+					<!-- end sidebar -->
+					<div style="clear: both;">&nbsp;</div>
+				</div>
+			<!-- end page -->
+			
+			<!-- start footer -->
+				
+			<!-- end footer -->
+</body>
+</html>
